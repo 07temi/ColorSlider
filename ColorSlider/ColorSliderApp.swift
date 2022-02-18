62382ca69f34c6f73af047ef7dc6a1b9158de83e
Art <artem_chernenko@ro.ru>
2022-02-18 14:52:15 +0300

    Initial Commit

---
+++ ColorSlider/ColorSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderApp.swift
+//  ColorSlider
+//
+//  Created by Артем Черненко on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
